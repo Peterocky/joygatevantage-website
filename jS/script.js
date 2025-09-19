dfb908886c996c79aa2c728f5cd55a4add016947
Peterocky <81236124+Peterocky@users.noreply.github.com>
2025-09-19 16:19:40 +0100

    Update script.js
    for toggle menu

--- jS/script.js
+++ jS/script.js
@@ -1,7 +1,6 @@
 function toggleMenu() {
   const nav = document.getElementById("navLinks");
   nav.classList.toggle("active");
-  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 }
 
 
