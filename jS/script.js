a0b29ddf6c35304871204383e961d7dc4c5410eb
Peterocky <81236124+Peterocky@users.noreply.github.com>
2025-09-19 10:09:00 +0100

    Update script.js

--- jS/script.js
+++ jS/script.js
@@ -1,6 +1,8 @@
 function toggleMenu() {
   const nav = document.getElementById("navLinks");
   nav.classList.toggle("active");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 }
+
 
 
